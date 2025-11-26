6b32217cc9ed31f1f739e4b5150ce72339047117
impressivecoder1 <rohanrahman251@gmail.com>
2025-11-27 01:18:41 +0600

    calling function added

--- js/first.js
+++ js/first.js
@@ -19,3 +19,23 @@ heartHeartCount('six')
 heartHeartCount('seven')
 heartHeartCount('eight')
 heartHeartCount('nine')
+// 
+// 
+// 
+
+    document.getElementById('num-one').addEventListener('click',function(){
+    const headerCoin =parseInt(document.getElementById('header-coin').innerText) ;
+    const twenty= 20
+    const nowHeaderCoin = headerCoin - twenty
+    const firstHeader = document.getElementById('first-div-header').innerText;
+    const firstDivNumber = document.getElementById('first-div-number').innerText;
+    console.log(firstHeader,firstDivNumber)
+     if(headerCoin>20){
+        alert(`Calling ${firstHeader} service ${firstDivNumber}.....`)
+        document.getElementById('header-coin').innerText = nowHeaderCoin
+    }
+    else{
+        alert('You need more than 20 coins to call.')
+    }
+})
+
