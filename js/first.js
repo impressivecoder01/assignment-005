8bd30f8e2a1d5f3d786da69b3c33d33c74ad20da
impressivecoder1 <rohanrahman251@gmail.com>
2025-11-26 23:34:17 +0600

    added heart in header done

--- js/first.js
+++ js/first.js
@@ -1,1 +1,21 @@
-console.log('added')
+// console.log('added')
+function heartHeartCount(id){
+    document.getElementById(id).addEventListener('click', function(){
+    // const heart = document.getElementsByClassName('div-heart')
+    // console.log(heart)
+    const count = 1;
+    const headerHeartNumber =parseInt(document.getElementById('header-heart').innerText) ;
+    const add = count + headerHeartNumber;
+    return document.getElementById('header-heart').innerText = add;
+    
+})
+}
+heartHeartCount('one')
+heartHeartCount('two')
+heartHeartCount('three')
+heartHeartCount('four')
+heartHeartCount('five')
+heartHeartCount('six')
+heartHeartCount('seven')
+heartHeartCount('eight')
+heartHeartCount('nine')
